f21a883fb4265e7c64c633288d6defb1ac62581b
Ragul Kumar <Ragul.Kumar@fisglobal.com>
2016-03-24 12:49:32 -0400

    Added dev seed project

---
+++ app_101/scripts/app.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Ragul.Kumar on 3/24/2016.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app_101/views/app.html'
+})
+export class AppComponent {
+  public message:string = 'Gul is learning Angular2';
+
+}
