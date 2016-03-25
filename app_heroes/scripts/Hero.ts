dbe76b5dd51850a991a44c492e826a7fbd4922bb
Ragul Kumar <Ragul.Kumar@fisglobal.com>
2016-03-25 10:21:06 -0400

    Implemented the heroes use case

---
+++ app_heroes/scripts/Hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Ragul.Kumar on 3/24/2016.
+ */
+interface Hero {
+  id: number;
+  name: string;
+}
---
+++ app_heroes/scripts/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Ragul.Kumar on 3/24/2016.
+ */
+
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
